021c120be6df7393cd0adca98bb9afb05ca30103
absdarekar <absdarekar01@gmail.com>
2020-08-12 19:17:33 +0530

    build: authenticate
    add:
    - prompts for success and failure of authenticate
    change:
    - path of files

--- bin/Authenticate.py
+++ bin/Authenticate.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets;
 import sys;
 import os;
-sys.path.insert(1, "/home/ubuntu/Documents/odrive-x")
+sys.path.insert(1, "/home/ubuntu/odrive-x")
 from gui.AuthenticateGui import AuthenticateGui;
 class Authenticate():
     def centering(self,arg_window):
@@ -20,6 +20,19 @@ class Authenticate():
         authkey=self.obj_AuthenticateGui.authkey.text();
         os.makedirs("/home/ubuntu/.odrive-x", exist_ok=True);
         os.system("sudo odrive authenticate "+authkey+" 2>&1 |tee /home/ubuntu/.odrive-x/prompt.txt");
+        with open("/home/ubuntu/.odrive-x/prompt.txt") as prompt_f:
+            if(prompt_f.read(5)=="Hello"):
+                success=QtWidgets.QMessageBox();
+                success.setWindowTitle("Welcome");
+                success.setText(prompt_f.readline().rstrip("\n"));
+                success.exec_();
+                self.obj_QMainWindow__validate.close();
+            else:
+                prompt_f.seek(0);
+                error=QtWidgets.QMessageBox();
+                error.setWindowTitle("Error");
+                error.setText(prompt_f.readline().rstrip("\n"));
+                error.exec_();
 if __name__ == "__main__":
     obj_QApplication=QtWidgets.QApplication(sys.argv)
     obj_Authenticate=Authenticate();
