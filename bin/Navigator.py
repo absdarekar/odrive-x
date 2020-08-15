93efe6941088facb0e08d91deafecbdfa2e62947
absdarekar <absdarekar01@gmail.com>
2020-08-16 00:40:12 +0530

    feat: navigator
    add:
    - GUI for navigator
    - extract current URL on clicking select

---
+++ bin/Navigator.py
@@ -0,0 +1,26 @@
+from PyQt5 import QtCore, QtGui, QtWidgets;
+from PyQt5.QtCore import QUrl
+import sys;
+import os;
+sys.path.insert(1, "/home/ubuntu/odrive-x");
+from gui.NavigatorGui import NavigatorGui;
+class Navigator():
+    def centering(self,arg_window):
+        window=arg_window.frameGeometry();
+        center=QtWidgets.QDesktopWidget().availableGeometry().center();
+        window.moveCenter(center);
+        arg_window.move(window.topLeft());
+    def navigate(self):
+        self.obj_QMainWindow__navigate=QtWidgets.QMainWindow();
+        self.centering(self.obj_QMainWindow__navigate);
+        self.obj_NavigatorGui=NavigatorGui();
+        self.obj_NavigatorGui.setupUi(self.obj_QMainWindow__navigate);
+        self.obj_QMainWindow__navigate.show();
+        self.obj_NavigatorGui.pushButton.clicked.connect(self.extract);
+    def extract(self):
+        url=str(self.obj_NavigatorGui.web.url());
+if __name__ == "__main__":
+    obj_QApplication=QtWidgets.QApplication(sys.argv);
+    obj_Navigator=Navigator();
+    obj_Navigator.navigate();
+    sys.exit(obj_QApplication.exec_());
