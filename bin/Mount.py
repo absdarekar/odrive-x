f0caf5b3245d2dd18694457deb807cdb36f31cf6
absdarekar <absdarekar01@gmail.com>
2020-08-21 11:56:07 +0530

    build: mount
    add:
    - embed navigator in mount
    remove:
    - navigator class

--- bin/Mount.py
+++ bin/Mount.py
@@ -4,7 +4,7 @@ import sys;
 import os;
 sys.path.insert(1, "/home/ubuntu/odrive-x")
 from gui.MountGui import MountGui;
-from bin.Navigator import Navigator;
+from gui.NavigatorGui import NavigatorGui;
 class Mount():
     def centering(self,arg_window):
         window=arg_window.frameGeometry();
@@ -17,17 +17,29 @@ class Mount():
         self.obj_MountGui=MountGui();
         self.obj_MountGui.setupUi(self.obj_QMainWindow__mount);
         self.obj_QMainWindow__mount.show();
-        self.remote_dir=self.obj_MountGui.remote_path.text();
         self.obj_MountGui.btn_local.clicked.connect(self.local);
-        self.obj_MountGui.btn_cloud.clicked.connect(self.cloud);
+        self.obj_MountGui.btn_cloud.clicked.connect(self.navigate);
         self.obj_MountGui.btn_mount.clicked.connect(self.mount);
+    def navigate(self):
+        self.obj_QMainWindow__navigate=QtWidgets.QMainWindow();
+        self.centering(self.obj_QMainWindow__navigate);
+        self.obj_NavigatorGui=NavigatorGui();
+        self.obj_NavigatorGui.setupUi(self.obj_QMainWindow__navigate);
+        self.obj_QMainWindow__navigate.show();
+        self.obj_NavigatorGui.web.loadProgress.connect(self.statusLoading);
+        self.obj_NavigatorGui.web.loadFinished.connect(self.statusDone);
+        self.obj_NavigatorGui.pushButton.clicked.connect(self.extract);
+    def extract(self):
+        self.remote_dir=str(self.obj_NavigatorGui.web.url());
+        self.obj_MountGui.remote_path.setText(self.remote_dir);
+        self.obj_QMainWindow__navigate.close();
+    def statusLoading(self):
+        self.obj_NavigatorGui.statusBar.showMessage("Loading...");
+    def statusDone(self):
+        self.obj_NavigatorGui.statusBar.showMessage("Done",1000);
     def local(self):
         self.local_dir=QFileDialog.getExistingDirectory(None, 'Select a directory',"/home");
         self.obj_MountGui.local_path.setText(self.local_dir);
-    def cloud(self):
-        self.obj_Navigator=Navigator();
-        self.remote_dir="/"; #temperory
-        self.obj_MountGui.remote_path.setText(self.remote_dir);
     def mount(self):
         os.system("sudo odrive mount "+self.local_dir+" "+self.remote_dir+" 2>&1 |tee /home/ubuntu/.odrive-x/mount.txt");
         with open("/home/ubuntu/.odrive-x/mount.txt") as mount_f:
